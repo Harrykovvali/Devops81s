b45f7e4ad5e6546a475fa6262a080f83e7e4e631
harrykovvali <harinik14@gmail.com>
2024-08-27 21:57:01 +0530

    conversation

--- Conversation.sh
+++ Conversation.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-Person1=Ramesh
-Person2=Suresh
-
-echo "$PERSON1::Hi $PERSON2, How are you ?"
-echo "$PERSON2::Hello $PERSON1, I am fine. How about you ?" 
-echo "$PERSON1::I am fine too $PERSON2,How's everything?"
-echo "$PERSON2::All good"
+PERSON1=RAMESH
+PERSON2=SURESH 
+echo "{$PERSON1}:: hoW ARE YOU {PERSON2}"
