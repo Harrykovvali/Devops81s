a1b3949f0af80e1720f3a3a9c6bc385690f96555
harrykovvali <harinik14@gmail.com>
2024-08-27 18:46:30 +0530

    Variable

---
+++ Conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Person1=Ramesh
+Person2=Suresh
+
+echo "$PERSON1:: Hi $PERSON2, How are you ?"
+echo "$PERSON2::Hello $PERSON1, I am fine. How about you ?
+echo "$PERSON1:: I am fine too $PERSON2,How's everything?"
+echo "$PERSON2:: All good!, $PERSON1"
