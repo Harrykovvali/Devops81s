ad5cac1c8f3aaee20426950bcc87a5a1b258ea00
harrykovvali <harinik14@gmail.com>
2024-08-28 06:12:47 +0530

    new file

---
+++ array.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+family=("mother","father","brother","sister")
+echo "The head of the family is :: ${family[0]}"
+echo "The second member in the family is :: ${family[1]}"
+echo "the family is::${family[@]}"
