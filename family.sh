77b2c395eddf39005e7ea06b38ac7232803a7e98
harrykovvali <harinik14@gmail.com>
2024-08-28 05:48:14 +0530

    family

--- family.sh
+++ family.sh
@@ -4,5 +4,5 @@ family_member2=$2
 echo "family_member1 is ::$family_member1"
 echo "family_member2 is :: $family_member2"
 echo "please enter family_member3"
-read -m family_member3
+read -s family_member3
 echo "family_member is :: $family_member3"
