a80e64e35f32f6f20a6be1d3f6f802fb7fbc05fc
harrykovvali <harinik14@gmail.com>
2024-08-28 05:43:05 +0530

    family

--- family.sh
+++ family.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 family_member1=$1
 family_member2=$2
-echo "Please enter $family_member1"
-echo" Please enter $family_member2"
+echo "family_member1 is ::$family_member1"
+echo "family_member2 is :: $family_member2"
