00bcefdbeebd1dcc5686e3fab6a4fef32612f448
harrykovvali <harinik14@gmail.com>
2024-08-29 05:52:47 +0530

    added to installation

--- installation.sh
+++ installation.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 Userid=$(id -u)
-echo $userid
+echo $Userid
+
+if [ userid -ne 0 ]
+then
+   echo " not a root user.exit"
+   exit 1
+fi 
+
+dnf list installed git 
+ 
+if [$? -ne 0 ]
+then 
+      echo "git is not installed, going to install"
+      dnf install git -y
+      if [ $? -ne 0]
+      then 
+         echo "git installation is not successful. please check"
+         exit 1
+      else
+         echo " git installation is successful"
+else
+         echo"git is already installed,nothing to do"
