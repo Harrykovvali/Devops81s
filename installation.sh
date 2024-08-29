3aba3883da6dd3046452769d2a9ce6b22dbdf23b
harrykovvali <harinik14@gmail.com>
2024-08-29 06:06:48 +0530

    added to installation

--- installation.sh
+++ installation.sh
@@ -13,13 +13,7 @@ dnf list installed git
 if [ $? -ne 0 ]
 then 
       echo "git is not installed, going to install"
-      dnf install git -y
-      if [ $? -ne 0 ]
-      then 
-         echo "git installation is not successful. please check"
-         exit 1
-      else
-         echo " git installation is successful"
+      
 else
-         echo "git is already installed,nothing to do"
+      echo "git is already installed,nothing to do"
 fi
