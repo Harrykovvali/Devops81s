241b5a4a5a8cdc44c6a9c25a0ddf9b922743a54f
harrykovvali <harinik14@gmail.com>
2024-08-29 06:13:16 +0530

    added to installation

--- installation.sh
+++ installation.sh
@@ -25,3 +25,29 @@ then
 else
       echo "git is already installed,nothing to do"
 fi
+userid=$(id -u)
+echo $userid
+
+if [ $userid -ne 0 ]
+then
+   echo " not a root user.exit"
+   exit 1
+fi 
+
+dnf list installed mysql 
+ 
+if [ $? -ne 0 ]
+then 
+      echo "mysql is not installed, going to install"
+      dnf install mysql -y
+
+      if [ $? -ne 0 ]
+      then 
+         echo "nysql installation is not successful. please check"
+         exit 1
+      else
+         echo " mysql installation is successful"
+      fi
+else
+      echo "mysql is already installed,nothing to do"
+fi
