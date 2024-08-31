85e1fb916f5bbcc261a5c5ef24d560390c3c6da7
harrykovvali <harinik14@gmail.com>
2024-08-31 06:55:37 +0530

    added to installation

--- colours.sh
+++ colours.sh
@@ -29,14 +29,7 @@ CHECK_ROOT
 
 dnf list installed git
 
-if [ $? -ne 0 ]
-then
-    echo "Git is not installed, going to install it.."
-    dnf install git -y
-    VALIDATE $? "Installing Git"
-else
-    echo "Git is already installed, nothing to do.."
-fi
+
 
 dnf list installed mysql
 
