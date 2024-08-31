69a5c650a65c54620c6c53a5a357b7f53121d355
harrykovvali <harinik14@gmail.com>
2024-08-31 17:37:25 +0530

    ExpenseProject.sh

--- ExpenseProject.sh
+++ ExpenseProject.sh
@@ -21,3 +21,29 @@ CHECK_ROOT()
 }
 CHECK_ROOT
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is...FAILED"
+        exit 1
+    else
+        echo "$2 is...SUCCESS"
+    fi
+}
+
+for package in $@
+do 
+ dnf list installed $package
+ if [ $? -ne 0 ]  
+    then
+       echo "$package is not installed, going to install it.."
+       dnf install $package -y
+       VALIDATE $? "Installing package"
+    else
+       echo "Git is already installed, nothing to do.."
+  fi
+done
+
+
+
+
