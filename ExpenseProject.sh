e6c88018d883463ecf460b49098c9d9772866ff5
harrykovvali <harinik14@gmail.com>
2024-08-31 17:25:37 +0530

    ExpenseProject.sh

--- ExpenseProject.sh
+++ ExpenseProject.sh
@@ -1,11 +1,16 @@
 USERID=$(id -u)
 #echo $USERID
+R="\u001b[31m"
+G="\u001b[32m"
+Y="\u001b[33m"
+N="\u001b[0m"
+
 
 CHECK_ROOT()
 {
 	if [ $USERID -ne 0 ]
 	then
-	     echo " Please run command under root privs"
+	     echo -e " $R Please run command under root privs $N"
 	fi
 
 }
