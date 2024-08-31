4df5865d57f31822168327a024c04db43b95dc41
harrykovvali <harinik14@gmail.com>
2024-08-31 16:17:06 +0530

    redirector.sh

--- redirector.sh
+++ redirector.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 LOGS_FOLDER="/var/log/shell-script"
+mkdir -p $LOGS_FOLDER
 SCRIPT_NAME=$(echo $0|cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER
+echo $LOG_FILE
+
 
 USERID=$(id -u)
 R="\e[31m"
