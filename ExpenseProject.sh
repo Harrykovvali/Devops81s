1c26814c22092a22ad8f227489d5120dd1ddf934
harrykovvali <harinik14@gmail.com>
2024-08-31 17:28:33 +0530

    ExpenseProject.sh

--- ExpenseProject.sh
+++ ExpenseProject.sh
@@ -5,12 +5,17 @@ G="\u001b[32m"
 Y="\u001b[33m"
 N="\u001b[0m"
 
+LOGS_FOLDER="/var/log/shell-script"
+mkdir -p $LOGS_FOLDER
+SCRIPT_NAME=$(echo $0|cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 
 CHECK_ROOT()
 {
 	if [ $USERID -ne 0 ]
 	then
-	     echo -e " $R Please run command under root privs $N"
+	     echo -e " $R Please run command under root privs $N " | tee -a $LOG_FILE
 	fi
 
 }
