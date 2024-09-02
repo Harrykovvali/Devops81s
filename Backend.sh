d0f0e25cdf10401b849e45366be3073ca5b30e6c
harrykovvali <harinik14@gmail.com>
2024-09-03 05:26:42 +0530

    Backend.sh

--- Backend.sh
+++ Backend.sh
@@ -5,22 +5,25 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
+
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
         echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
         exit 1
     fi
+}
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
-        echo $2
         exit 1
     else
         echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
@@ -32,23 +35,26 @@ echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 CHECK_ROOT
 
 dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "diabling older version of nodes" 
+VALIDATE $? "Disable default nodejs"
+
 dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "enable latest version of nodes"
+VALIDATE $? "Enable nodejs:20"
+
 dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "install nodejs"
- 
-id expense &>>LOG_FILE
-if [ &? -ne 0 ]
-then 
-   echo -e "expense user doesnt exist..adding now"
-   user add expense &>>LOG_FILE
-   VALIDATE $? "creating useradd expense "
-else 
-   ech0 -e "expense user already created $Y ..skipping $N"
+VALIDATE $? "Install nodejs"
+
+id expense &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo -e "expense user not exists... $G Creating $N"
+    useradd expense &>>$LOG_FILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "expense user already exists...$Y SKIPPING $N"
 fi
- mkdir  -p /app
- VALIDATE $? "Creating /app folder"
+
+mkdir -p /app
+VALIDATE $? "Creating /app folder"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
 VALIDATE $? "Downloading backend application code"
@@ -58,10 +64,7 @@ rm -rf /app/* # remove the existing code
 unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code"
 
-cd /app
 npm install &>>$LOG_FILE
-VALIDATE $? " checking installation of npm"
-
 cp /home/ec2-user/Devops81s/backend.service /etc/systemd/system/backend.service
 
 # load the data before running backend
@@ -69,27 +72,7 @@ cp /home/ec2-user/Devops81s/backend.service /etc/systemd/system/backend.service
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h mysql.trydevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
-VALIDATE $? "Schema loading"
-
-systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "Daemon reload"
-
-systemctl enable backend &>>$LOG_FILE
-VALIDATE $? "Enabled backend"
-
-systemctl restart backend &>>$LOG_FILE
-VALIDATE $? "Restarted Backend"
-
-npm install &>>$LOG_FILE
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
-
-# load the data before running backend
-
-dnf install mysql -y &>>$LOG_FILE
-VALIDATE $? "Installing MySQL Client"
-
-mysql -h mysql.trydevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+mysql -h mysql.daws81s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
 VALIDATE $? "Schema loading"
 
 systemctl daemon-reload &>>$LOG_FILE
