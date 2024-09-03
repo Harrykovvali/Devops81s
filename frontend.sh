0f2b1faf56c70d14a7cd0549568964d314b6732c
harrykovvali <harinik14@gmail.com>
2024-09-03 06:38:41 +0530

    2 files

--- frontend.sh
+++ frontend.sh
@@ -53,7 +53,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Extract frontend code"
 
-cp /home/ec2-user/DevOps81s/expense.conf /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/DevOps81s/Expense.conf /etc/nginx/default.d/expense.conf
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOG_FILE
