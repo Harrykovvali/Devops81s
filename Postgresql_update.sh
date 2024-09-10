a669cc9e0cad0e1f9b0dbb76af1391a3c0c6836a
harrykovvali <harinik14@gmail.com>
2024-09-10 11:12:04 +0530

    POS

--- Postgresql_update.sh
+++ Postgresql_update.sh
@@ -8,10 +8,14 @@ CHECK_ROOT(){
 CHECK_ROOT
 Version=$(systemctl status postgresql-16)
 
+#check if postgresql-16 is already installed 
+
 if [ $? -eq 0 ]
 then
     echo "Postgresql-16 is already installed"
     exit 1
 else
     echo "proceeding with upgrade to Postgresql-16"
+
 fi
+sudo su - postgres
