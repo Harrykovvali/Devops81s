ece27bebcdfaca3be6d2b08529e81945e8ced6bc
harrykovvali <harinik14@gmail.com>
2024-09-10 06:14:12 +0530

    POS

---
+++ Postgresql_update.sh
@@ -0,0 +1,17 @@
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+CHECK_ROOT
+Version=$(systemctl status postgresql-16)
+
+if [ $? -eq 0 ]
+then
+    echo "Postgresql-16 is already installed"
+    exit 1
+else
+    echo "proceeding with upgrade to Postgresql-16"
+fi
